ee2540929c856aa34de567b70e44090c7204c42c
mi1qw <123mib@gmail.com>
2023-03-12 20:25:16 +0200

    SpringDI

---
+++ src/main/java/com/example/di/SpringDI.java
@@ -0,0 +1,19 @@
+package com.example.di;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringDI {
+    public static void main(final String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext();
+        context.register(Store.class);
+        context.register(StartUI.class);
+        context.register(ConsoleInput.class);
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
+        ui.add("Petr Arsentev");
+        ui.add("Ivan ivanov");
+        ui.input();
+        ui.print();
+    }
+}
